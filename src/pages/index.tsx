6db3a524f8160aa658b063dd97a7bb6c497ab8f1
Vladimir Revyagin <skorabeyf@gmail.com>
2024-10-03 21:22:35 +0300

    feat(leaveBtn): UI компонент, не работает функц

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import { EventCard } from '@/entities/event'
 import { JoinEventButton } from '@/features/join-event'
+import { LeaveEventButton } from '@/features/leave-event'
 import { trpc } from '@/shared/api'
 import { NavBar } from '@/widget/navbar'
 import { useSession } from 'next-auth/react'
@@ -18,7 +19,9 @@ export default function Home() {
             <EventCard
               {...event}
               action={
-                !event.isJoined && (
+                event.isJoined ? (
+                  <LeaveEventButton eventId={event.id} onSuccess={refetch} />
+                ) : (
                   <JoinEventButton eventId={event.id} onSuccess={refetch} />
                 )
               }
