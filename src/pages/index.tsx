ad0e251c4d638cbbb552b41ea25964bb0353508a
Vladimir Revyagin <skorabeyf@gmail.com>
2024-10-03 17:48:14 +0300

    feat(ui navbar): Создание  навигации

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,29 @@
-import { EventCard } from "@/entities/event";
-import { JoinEventButton } from "@/features/join-event";
-import { trpc } from "@/shared/api";
+import { EventCard } from '@/entities/event'
+import { JoinEventButton } from '@/features/join-event'
+import { trpc } from '@/shared/api'
+import { NavBar } from '@/widget/navbar'
+import React from 'react'
 
 export default function Home() {
-  const { data, refetch } = trpc.event.findMany.useQuery();
+  const { data, refetch } = trpc.event.findMany.useQuery()
 
   return (
-    <ul>
-      {data?.map((event) => (
-        <li key={event.id} className="mb-6">
-          <EventCard
-            {...event}
-            action={
-              !event.isJoined && (
-                <JoinEventButton eventId={event.id} onSuccess={refetch} />
-              )
-            }
-          />
-        </li>
-      ))}
-    </ul>
-  );
+    <>
+      <NavBar />
+      <ul>
+        {data?.map((event) => (
+          <li key={event.id} className="mb-6">
+            <EventCard
+              {...event}
+              action={
+                !event.isJoined && (
+                  <JoinEventButton eventId={event.id} onSuccess={refetch} />
+                )
+              }
+            />
+          </li>
+        ))}
+      </ul>
+    </>
+  )
 }
