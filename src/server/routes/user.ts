e83b992d14e26c7b7a3302f4d85877a232eb5959
Vladimir Revyagin <skorabeyf@gmail.com>
2024-10-24 09:24:05 +0300

    Проверка при регистрации

--- src/server/routes/user.ts
+++ src/server/routes/user.ts
@@ -3,7 +3,13 @@ import { procedure, router } from '../trpc'
 import { prisma } from '../db'
 
 export const userRouter = router({
-  createUser: procedure.input(CreateUserSchema).mutation(({ input }) => {
+  createUser: procedure.input(CreateUserSchema).mutation(async ({ input }) => {
+    const existUser = await prisma.user.findUnique({
+      where: { email: input.email },
+    })
+    if (existUser) {
+      throw new Error('User already exist')
+    }
     return prisma.user.create({
       data: input,
     })
